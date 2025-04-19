b38a219cca29e717660f4a9977868e214596f4fa
Aditya Patel <adityakdpatel@gmail.com>
2025-04-19 11:11:16 -0400

    feat: added the concept of distance for the bfs graph

--- bfs_graph.py
+++ bfs_graph.py
@@ -19,28 +19,49 @@ graph = {
 #   \     / \
 #    6 - 7   8
 
+graph2 = {
+    1: [2, 3],
+    2: [1, 3, 4, 5],
+    3: [1, 2, 5, 7, 8],
+    4: [2],
+    5: [2, 3, 6],
+    6: [5],
+    7: [3, 8],
+    8: [3, 7]
+}
 
-def bfs(graph, start) -> list:
+#    7───8
+#     \ /
+#      3───5───6
+#     / \
+#    1───2───4
+
+def bfs(graph, start):
     queue = collections.deque()
-    queue.append(start)
+    if start:
+        queue.append(start)
     output = []
-
     status = {}
+    dist = collections.defaultdict(int)
+    dist[start] = 0
     for node in graph.keys():
         status[node] = "white"
-
     while queue:
-        for i in range(len(queue)):
+        for _ in range(len(queue)):
             node = queue.popleft()
             if status[node] != "black":
                 status[node] = "red"
                 output.append(node)
-                for child in graph[node]:
-                    if status[child] == "white":
-                        status[child] = "red"
-                        queue.append(child)
+                for child_node in graph[node]:
+                    if status[child_node] == "white":
+                        dist[child_node] += dist[node] + 1
+                        status[child_node] = "red"
+                        queue.append(child_node)
                 status[node] = "black"
+    print(output)
+    print(dist)
     return output
 
-
-print(bfs(graph, 1))
+if __name__ == "__main__":
+    bfs(graph, 1)
+    bfs(graph2, 1)
